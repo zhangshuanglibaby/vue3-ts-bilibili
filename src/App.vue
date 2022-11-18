35836fd78ee8d3aa26f32633f9cd00ba25d44d01
unknown <943165920@qq.com>
2022-11-18 12:57:18 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,4 @@ export default defineComponent({
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
