2566d15c1116ca4d62a881ede5f9c58aa5fdb2e3
zhangshuangli <943165920@qq.com>
2022-11-21 22:28:25 +0800

    编写appHeader组件

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,20 +1,69 @@
 <!--
  * @Date: 2022-11-21 20:30:56
  * @LastEditors: zhangshuangli
- * @LastEditTime: 2022-11-21 20:36:31
+ * @LastEditTime: 2022-11-21 22:27:37
  * @Description: 这是****文件
 -->
 <template>
-  <header>
+  <header class="app-header">
     <!-- logo -->
-    <i class="iconfont icon-shoucangxuanzhong"></i>
+    <div class="logo">
+      <i class="iconfont icon-bilibili"></i>
+    </div>
     <!-- 搜索 -->
+    <div class="search">
+      <i class="iconfont icon-sousuo"></i>
+    </div>
     <!-- 头像 -->
+    <div class="face">
+      <img :src="require('../assets/images/login.png')" alt="">
+    </div>
     <!-- 下载按钮 -->
+    <div class="down-app">
+      <a href="https://dl.hdslb.com/mobile/fixed/bili_win/bili_win-install.exe?t=20220722&spm_id_from=333.47.b_646f776e6c6f61642d6c696e6b.9">
+        下载app
+      </a>
+    </div>
   </header>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="less">
+.app-header {
+  display: flex;
+  align-items: center;
+  background: #fff;
+  padding: 16px 24px;
+  .logo {
+    flex: 1;
+    .icon-bilibili {
+      color: #fb7299;
+      font-size: 56px;
+    }
+  }
+  .search {
+    .icon-sousuo {
+      color: #ccc;
+      font-size: 44px;
+    }
+  }
+  .face {
+    padding: 0 30px;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+  }
+  .down-app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    color: #fff;
+    background-color: #fb7299;
+    padding: 10px 20px;
+    border-radius: 10px;
+  }
+}
 </style>
